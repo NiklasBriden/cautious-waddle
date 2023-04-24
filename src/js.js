fb4cff80c10edac5491982c14c901c7eb665e613
NiklasBriden <126027728+NiklasBriden@users.noreply.github.com>
2023-04-24 12:18:23 +1200

    hello

---
+++ src/js.js
@@ -0,0 +1,16 @@
+//Create a function that takes a boolean variable and return it as a string
+//ex: toString(true) -> "true"
+
+//Create a function that takes two strings as args and return either true or false depending on the total number of chars
+//in the first string is equal to the total number chars in the second string.code-active-line
+
+//ex: compareTwoStrings("AB", "CD") -> true
+//ex: compareTwoStrings("AB", "DE") -> false
+
+function compareTwoStrings(strOne, strTwo) {
+    return strOne.length == strTwo.length
+}
+
+//Create a function for the game rock, paper, scissors.
+//ex: playGame("Rock", "Paper") -> "Winner is Paper"
+
